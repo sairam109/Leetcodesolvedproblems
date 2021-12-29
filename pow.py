87190ba54df4fcf4e746ff0e354e6e7165fc1bb3
sairamcoder01 <35161983+sairam109@users.noreply.github.com>
2021-12-29 15:28:45 +0530

    Create pow.py

---
+++ pow.py
@@ -0,0 +1,6 @@
+//pow of n- https://leetcode.com/problems/powx-n/submissions/
+  // runtime- 51ms memory-14mb
+import math
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return math.pow(x,n)
